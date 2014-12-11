dcf36479ad13e78c414166ac064820763cd8f46d
Feifei HANG <l.daniel.hung@gmail.com>
2014-12-11 17:34:35 +0000

    little navigation bar:smirk::smirk::smirk:

--- js/main.js
+++ js/main.js
@@ -126,3 +126,18 @@ $ch.use(['./chop-bundle'], function () {
   // Apply `PACKS` on inline template.
   $ch.find('#components').inline(PACKS);
 });
+
+
+// jQuery smooth scroll.
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 500);
+      return false;
+    }
+  }
+});
